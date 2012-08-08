b7562b6609deafa3fa8a9083951e754cc9529f9b
Elliott Williams <elliott@openmrs.org>
2012-08-08 13:13:54 -0400

    Admin interface improvements. Release for OpenMRS ID v1.4

--- resource/groups-admin.js
+++ resource/groups-admin.js
@@ -4,8 +4,10 @@ $().ready(function(){
 		$.ajax('/admin/groups/provisioning', {
 			timeout: 5000,
 			success: function(data){
-				// update the status
-				$('#provisioning-status').html(data);
+				$('#provisioning-status').hide().html(data); // parse new HTML and hide status (while processing)
+				// remove everything except what we know is the status
+				var keep = $('#provisioning-status').children('#provisioning-status-xhr');
+				$('#provisioning-status').html(keep).show(); // replace with ONLY the status, and make visible
 			}
 		});
 	}, 5000);
